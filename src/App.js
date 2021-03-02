da9a68a8ab1c836db7a450343902f036f712914c
Harumi Terayama <hterayama@gmail.com>
2021-03-02 20:18:07 +0100

    iteration 1 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React from "react";
+import Table from "./component/table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sampleArray: contacts.slice(0, 5),
+    };
+  }
+
+  render() {
+    return (
+      <div className="IronContact">
+        <h1>IronContacts</h1>
+        <div className="table">
+          <table>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            {this.state.sampleArray.map((contact) => {
+              return (
+                <tr key={contact.id}>
+                  <td>
+                    <img
+                      className="contact-picture"
+                      src={contact.pictureUrl}
+                    ></img>
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+        ;
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/table.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import contacts from "./../contacts.json";
+
+// class Table extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       contacts: contacts.slice(0, 5),
+//     };
+//   }
+//   render() {
+//     return;
+
+//   }
+// }
+
+// export default Table;
